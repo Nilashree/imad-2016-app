3c6f53ec30516d5ea7e4f250852124bfafeaa843
Nilashree <nilashree.shirodkar@gmail.com>
2016-10-05 16:44:35 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/example-one.html', function (req, res) {
+app.get('/example-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'example-one.html'));
 });
 app.get('/ui/style.css', function (req, res) {
