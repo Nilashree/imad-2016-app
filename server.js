16c838bd58a0505e6896cdbb2613b6f3f3a37e09
Nilashree <nilashree.shirodkar@gmail.com>
2016-10-07 18:29:48 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,70 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var exampleone={
+    title:'Example-one|Nilashree Shirodkar',
+    heading:'Example-one',
+    date:'07/10/2016',
+    content: 
+        `<p>
+        Hello everyone !! This is my first html example.Hello everyone !! This is my first html example.
+        Hello everyone !! This is my first html example.Hello everyone !! This is my first html example.
+        </p>   
+    <p>
+        I am enjoying learning it.I am enjoying learning it.I am enjoying learning it.I am enjoying learning it.
+        I am enjoying learning it.I am enjoying learning it.I am enjoying learning it.I am enjoying learning it.
+        I am enjoying learning it.I am enjoying learning it.I am enjoying learning it.I am enjoying learning it.
+    </p>
+    <p>
+    Trying to be a web developer soon.Trying to be a web developer soon.Trying to be a web developer soon.Trying to be a web developer soon.Trying to be a web developer soon.
+     Trying to be a web developer soon.Trying to be a web developer soon.Trying to be a web developer soon.     
+    </p>`
+};
+
+function createtemplate(data)
+{
+    
+var date=data.date;
+var heading=data.geading;
+var title=data.title;
+var content=data.content;
+var htmltemplate=
+    `<html>
+<head>
+<title>
+   ${title}
+</title> 
+    <meta name="viewport" content="width=device-width,initial_scale=1"/> 
+    <link href="/ui/style.css" rel="stylesheet" />
+
+</head>
+
+<body>
+    <div>
+<a href="/"> Home </a>    
+<hr/>
+
+</div>
+<div class="displayer" style="background-color:lightgreen">
+<h2 >
+
+${heading}    
+</h2>
+<div>
+   ${date}
+</div>
+
+    <div>
+  ${content}
+   </div>
+</div>
+</body>
+</html>
+
+    `;
+    return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
